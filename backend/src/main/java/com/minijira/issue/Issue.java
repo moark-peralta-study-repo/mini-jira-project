a5878feab6d8b9e536bb3fccd53a9953316094bc
moark-peralta-study-repo <markperaltaasd@gmail.com>
2026-03-11 23:33:29 +0800

    save point Issue entity / Status enum

---
+++ backend/src/main/java/com/minijira/issue/Issue.java
@@ -0,0 +1,46 @@
+package com.minijira.issue;
+
+import org.springframework.data.annotation.Id;
+
+public class Issue {
+  @Id
+  private Long id;
+  private String title;
+  private String description;
+  private IssueStatus status;
+
+  public Issue() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public IssueStatus getStatus() {
+    return status;
+  }
+
+  public void setStatus(IssueStatus status) {
+    this.status = status;
+  }
+}
---
+++ backend/src/main/java/com/minijira/issue/IssueStatus.java
@@ -0,0 +1,7 @@
+package com.minijira.issue;
+
+public enum IssueStatus {
+  TODO,
+  IN_PROGRESS,
+  DONE
+}
